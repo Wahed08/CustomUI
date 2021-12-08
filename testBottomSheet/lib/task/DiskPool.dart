210b67c9190a53d4281afcdc8aa48b91b1c707cf
Wahed08 <wahedfarhad123@gmail.com>
2021-12-08 17:58:31 +0600

    chnages in itemcount

--- testBottomSheet/lib/task/DiskPool.dart
+++ testBottomSheet/lib/task/DiskPool.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class DiskPoolScreen extends StatefulWidget {
   const DiskPoolScreen({Key? key}) : super(key: key);
@@ -7,12 +8,19 @@ class DiskPoolScreen extends StatefulWidget {
   _DiskPoolScreenState createState() => _DiskPoolScreenState();
 }
 
-class _DiskPoolScreenState extends State<DiskPoolScreen> with SingleTickerProviderStateMixin{
+class _DiskPoolScreenState extends State<DiskPoolScreen>
+    with SingleTickerProviderStateMixin {
+  List images = ["image.jpg", "image.jpg", "image.jpg"];
 
-  List images = [
-      "image.jpg",
-      "image.jpg",
-      "image.jpg"
+   var colors = [
+    Colors.cyan,
+    Colors.deepOrange[200],
+    Colors.lightGreen,
+  ];
+    var firstName = [
+    "Dhaka",
+    "Singapore",
+    "Malayasia"
   ];
 
   @override
@@ -20,94 +28,196 @@ class _DiskPoolScreenState extends State<DiskPoolScreen> with SingleTickerProvid
     TabController _tabcontroller = TabController(length: 3, vsync: this);
     return Scaffold(
       body: Container(
+        color: Colors.purple[900],
         child: Column(
           children: [
             Padding(
-              padding: const EdgeInsets.only(left: 30,top: 50),
+              padding: const EdgeInsets.only(left: 30, top: 50),
               child: Align(
                 alignment: Alignment.centerLeft,
                 child: Container(
-                  child:Text("Disk Pool", style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),),
+                  child: Text(
+                    "Disk Pool",
+                    style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white),
+                  ),
                 ),
               ),
             ),
-            SizedBox(height: 40,),
+            SizedBox(
+              height: 40,
+            ),
             Container(
-              margin: EdgeInsets.only(bottom: 10),
+                margin: EdgeInsets.only(bottom: 10),
                 child: Align(
                   alignment: Alignment.centerLeft,
                   child: TabBar(
-                    labelColor: Colors.black,
+                    labelColor: Colors.white,
                     // isScrollable: true,
-                    labelPadding:EdgeInsets.only(left: 20, right: 20),
+                    labelPadding: EdgeInsets.only(left: 20, right: 20),
                     unselectedLabelColor: Colors.grey,
-                    indicator: CircleIndicator(color: Colors.black54, radius: 4),
+                    indicator: CircleIndicator(color: Colors.white, radius: 4),
                     controller: _tabcontroller,
-                              tabs: [
-                  Tab(text: "Places",),
-                  Tab(text: "Inspirations",),
-                  Tab(text: "Emotions",)
-                              ],
-                            ),
+                    tabs: [
+                      Tab(
+                          child: Text(
+                        "Places",
+                        style: GoogleFonts.oswald(
+                            fontSize: 17, fontWeight: FontWeight.w400),
+                      )),
+                      Tab(
+                        child: Text("Inspire",
+                            style: GoogleFonts.oswald(
+                                fontSize: 17, fontWeight: FontWeight.w400)),
+                      ),
+                      Tab(
+                        child: Text("Emotions",
+                            style: GoogleFonts.oswald(
+                                fontSize: 17, fontWeight: FontWeight.w400)),
+                      )
+                    ],
+                  ),
                 )),
             Container(
-              margin: EdgeInsets.only(left: 10, right: 10),
+              margin: EdgeInsets.only(left: 10),
               width: double.maxFinite,
               height: 350,
-              child: TabBarView(
-                controller: _tabcontroller,
-                children: [
+              child: TabBarView(controller: _tabcontroller, children: [
                 ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 3,
-                  itemBuilder: (_, index){
-                  return Container(
-                    height: 300,
-                    width: 200,
-                    margin: EdgeInsets.only(right: 10, top: 10),
-                    decoration: BoxDecoration(
-                      // borderRadius: BorderRadius.circular(20),
-                      image: DecorationImage(
-                        image: AssetImage(
-                          "asset/"+images[index]
-                        )
-                      )
-                    ),
-                  );
-                }),
-                    ListView.builder(
-                  itemCount: 3,
-                  itemBuilder: (_, index){
-                  return Container(
-                    height: 300,
-                    width: 200,
-                    decoration: BoxDecoration(
-                      // borderRadius: BorderRadius.circular(20),
-                      image: DecorationImage(
-                        image: AssetImage(
-                          "asset/"+images[index]
-                        )
-                      )
-                    ),
-                  );
-                }),
-                    ListView.builder(
-                  itemCount: 3,
-                  itemBuilder: (_, index){
-                  return Container(
-                    height: 300,
-                    width: 200,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      image: DecorationImage(
-                        image: AssetImage(
-                          "asset/"+images[index]
-                        )
-                      )
-                    ),
-                  );
-                }),
-                
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 3,
+                    itemBuilder: (_, index) {
+                      return Container(
+                        margin: EdgeInsets.only(left: 10, right: 20),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              primary: colors[index]),
+                          onPressed: () {},
+                          child: Container(
+                            height: 300,
+                            width: 200,
+                            margin: EdgeInsets.only(top: 10),
+                            child: Column(
+                              children: [
+                                Container(
+                                    child: Column(
+                                  children: [
+                                    Text("${firstName[index]}",
+                                        style: GoogleFonts.odibeeSans(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 30)),
+                                    Text("Arena",
+                                        style: GoogleFonts.odibeeSans(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 25))
+                                  ],
+                                )),
+                                SizedBox(
+                                  height: 40,
+                                ),
+                                Container(
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Container(
+                                        decoration: BoxDecoration(
+                                            color: Colors.blueGrey,
+                                            boxShadow: [
+                                              BoxShadow(
+                                                  blurRadius: 10,
+                                                  color: Colors.black,
+                                                  offset: Offset(1, 3))
+                                            ],
+                                            borderRadius: BorderRadius.all(
+                                                Radius.circular(5.0))),
+                                        height: 50,
+                                        width: 200,
+                                        // color: Colors.blueGrey,
+                                        child: Center(
+                                            child: Text(
+                                          "Prize",
+                                          style: GoogleFonts.lato(fontSize: 20),
+                                        )),
+                                      ),
+                                      SizedBox(
+                                        height: 10,
+                                      ),
+                                      Container(
+                                        decoration: BoxDecoration(
+                                            color: Colors.blueGrey,
+                                            boxShadow: [
+                                              BoxShadow(
+                                                  blurRadius: 10,
+                                                  color: Colors.black,
+                                                  offset: Offset(1, 3))
+                                            ],
+                                            borderRadius: BorderRadius.all(
+                                                Radius.circular(5.0))),
+                                        height: 50,
+                                        width: 200,
+                                        child: Center(
+                                            child: Text(
+                                          "Player Online",
+                                          style: GoogleFonts.lato(fontSize: 20),
+                                        )),
+                                      ),
+                                      SizedBox(
+                                        height: 10,
+                                      ),
+                                      Container(
+                                        decoration: BoxDecoration(
+                                            color: Colors.blueGrey,
+                                            boxShadow: [
+                                              BoxShadow(
+                                                  blurRadius: 10,
+                                                  color: Colors.black,
+                                                  offset: Offset(1, 3))
+                                            ],
+                                            borderRadius: BorderRadius.all(
+                                                Radius.circular(5.0))),
+                                        height: 50,
+                                        width: 200,
+                                        child: Center(
+                                            child: Text(
+                                          "Entry Fee",
+                                          style: GoogleFonts.lato(fontSize: 20),
+                                        )),
+                                      ),
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    }),
+                ListView.builder(
+                    itemCount: 3,
+                    itemBuilder: (_, index) {
+                      return Container(
+                        height: 300,
+                        width: 200,
+                        decoration: BoxDecoration(
+                            // borderRadius: BorderRadius.circular(20),
+                            image: DecorationImage(
+                                image: AssetImage("asset/" + images[index]))),
+                      );
+                    }),
+                ListView.builder(
+                    itemCount: 3,
+                    itemBuilder: (_, index) {
+                      return Container(
+                        height: 300,
+                        width: 200,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            image: DecorationImage(
+                                image: AssetImage("asset/" + images[index]))),
+                      );
+                    }),
               ]),
             )
           ],
@@ -118,7 +228,7 @@ class _DiskPoolScreenState extends State<DiskPoolScreen> with SingleTickerProvid
 }
 
 // ignore: must_be_immutable
-class CircleIndicator extends Decoration{
+class CircleIndicator extends Decoration {
   final Color color;
   double radius;
 
@@ -126,24 +236,25 @@ class CircleIndicator extends Decoration{
 
   @override
   BoxPainter createBoxPainter([VoidCallback? onChanged]) {
-    return _CirclePainter(color:color, radius: radius);
+    return _CirclePainter(color: color, radius: radius);
   }
 }
 
-class _CirclePainter extends BoxPainter{
+class _CirclePainter extends BoxPainter {
   final double radius;
   late Color color;
 
   _CirclePainter({required this.radius, required this.color});
 
   @override
-  void paint(Canvas canvas, Offset offset, ImageConfiguration cfg){
+  void paint(Canvas canvas, Offset offset, ImageConfiguration cfg) {
     late Paint _paint;
 
     _paint = Paint()..color = color;
     _paint = _paint..isAntiAlias = true;
 
-    final Offset circleOffset = offset + Offset(cfg.size!.width / 2, cfg.size!.height - radius);
+    final Offset circleOffset =
+        offset + Offset(cfg.size!.width / 2, cfg.size!.height - radius);
     canvas.drawCircle(circleOffset, radius, _paint);
   }
 }
